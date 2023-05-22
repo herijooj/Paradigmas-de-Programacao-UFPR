2bc44cbb23727776d42c1a964939e41075dd1260
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-05-22 18:45:35 -0300

    [add] constructor && restricted property

--- jogo/Sector/Sector.java
+++ jogo/Sector/Sector.java
@@ -3,6 +3,14 @@ package Sector;
 public class Sector {
     private int i;
     private int j;
+    private boolean restricted;
+
+    public Sector(int i, int j) {
+        // will randomly assing a restricted sector
+        this.setRestricted(Math.random() < 0.5);
+        this.setI(i);
+        this.setJ(j);
+    }
 
     // getters
     public int getI() {
@@ -11,6 +19,10 @@ public class Sector {
 
     public int getJ() {
         return this.j;
+    }
+
+    public boolean isRestricted() {
+        return this.restricted;
     }
 
     // setters
@@ -26,6 +38,11 @@ public class Sector {
         if (j < 0)
             throw new IllegalArgumentException("J must be positive");
         this.j = j;
+        return;
+    }
+
+    public void setRestricted(boolean value) {
+        this.restricted = value;
         return;
     }
 
