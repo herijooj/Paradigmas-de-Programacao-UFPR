5ffcaa2c1a54dceabdfbcf00b6dc52e1fb374ded
Vinícius Fontoura <vfa20@inf.ufpr.br>
2023-05-23 20:12:44 -0300

    [tweak]

--- jogo/Board/Board.java
+++ jogo/Board/Board.java
@@ -1,15 +1,23 @@
 package Board;
 
+import java.util.List;
+
 import Coordinate.Coordinate;
 import Sector.Sector;
 
 public class Board {
     private int size;
     private Sector[][] board;
+    // creates a List of coordinates
+
+    private List<Coordinate> restrictedSectors;
+
     private Coordinate[] restrictedSectors;
+    private int restrictedSectorsCount;
 
     public Board(int size) {
         this.setSize(size);
+        this.setRestrictedSectorsCount(4);
         this.setRestrictedSectors();
         this.setBoard();
     }
@@ -23,11 +31,15 @@ public class Board {
         return this.board;
     }
 
-    public int getRestrictedSectors() {
+    public Coordinate[] getRestrictedSectors() {
+        return this.restrictedSectors;
+    }
+
+    public int getRestrictedSectorsCount() {
         return this.restrictedSectorsCount;
     }
-    // setters
 
+    // setters
     public void setSize(int size) {
         if (size < 0)
             throw new IllegalArgumentException("Size must be positive");
@@ -35,8 +47,24 @@ public class Board {
         return;
     }
 
+    public void setRestrictedSectorsCount(int restrictedSectorsCount) {
+        if (restrictedSectorsCount < 0)
+            throw new IllegalArgumentException("Restricted sectors count must be positive");
+        this.restrictedSectorsCount = restrictedSectorsCount;
+        return;
+    }
+
     public void setRestrictedSectors() {
-        this.restrictedSectorsCount = (int) Math.floor(Math.pow(this.getSize(), 2) / 3);
+        int restrictedSectorsCount = this.getRestrictedSectorsCount();
+        this.restrictedSectors = new Coordinate[restrictedSectorsCount];
+
+        // create the restricted sectors
+        for (int i = 0; i < restrictedSectorsCount; i++) {
+            // randomnly generate two numbers between 0 and 9
+            int iCoordinate = (int) (Math.random() * 10);
+            int jCoordinate = (int) (Math.random() * 10);
+            this.restrictedSectors[i] = new Coordinate(iCoordinate, jCoordinate);
+        }
         return;
     }
 
@@ -47,6 +75,9 @@ public class Board {
         // create the board
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
+                
+                if ()
+
                 boolean isRestricted = false;
                 this.board[i][j] = new Sector(i, j, isRestricted);
             }
