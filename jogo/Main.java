f8ce2a8f77f9ef6ec2b37d4b1f123e2e06dd3b18
Vinícius Fontoura <vfa20@inf.ufpr.br>
2023-05-23 15:24:45 -0300

    [tweak]

--- jogo/Main.java
+++ jogo/Main.java
@@ -4,11 +4,12 @@ import Sector.*;
 import Cores.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println(Cores.ANSI_RED + "Red text" + Cores.ANSI_RESET);
-        System.out.println(Cores.ANSI_BLUE + "Blue text" + Cores.ANSI_RESET);
-        System.out.println(Cores.ANSI_YELLOW_BACKGROUND + "Yellow background"
-                + Cores.ANSI_RESET);
+        // System.out.println(Cores.ANSI_RED + "Red text" + Cores.ANSI_RESET);
+        // System.out.println(Cores.ANSI_BLUE + "Blue text" + Cores.ANSI_RESET);
+        // System.out.println(Cores.ANSI_YELLOW_BACKGROUND + "Yellow background"
+        // + Cores.ANSI_RESET);
 
         Board board = new Board(9);
 
