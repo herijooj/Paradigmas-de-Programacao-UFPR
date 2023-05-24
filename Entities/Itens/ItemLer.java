db1397d29ecd5995b33fa2cc7c28d6c2800ab4ff
HeriJooj <heric_camargo@hotmail.com>
2023-05-24 20:33:46 -0300

    [add] template to the item classes

---
+++ Entities/Itens/ItemLer.java
@@ -0,0 +1,23 @@
+package Entities;
+
+// class
+public class ItemLer extends ItemCharacteristics {
+
+    // attributes
+    
+    // constructor
+    public ItemLer (Coordinate position) 
+    {
+        super(position);
+    }
+    
+    // getters
+    
+    // setters
+
+    // methods
+    public itemAbility() 
+    {
+        // TODO
+    }
+}
