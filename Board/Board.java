f06e5009a2966e26ac7862b78eacb4d7ca94b107
Pedro Amaral <amaral1@ufpr.br>
2023-05-25 08:53:08 -0300

    [fix] imports work

---
+++ Board/Board.java
@@ -0,0 +1,130 @@
+package Board;
+
+// imports 
+import java.util.List;
+import Entities.*; // adicionei pra importar as entidades - amaral
+
+// class
+public class Board {
+    private int size;
+    private Sector[][] board;
+    // creates a List of coordinates
+
+    //private List<Coordinate> restrictedSectors;
+
+    private Coordinate[] restrictedSectors;
+    private int restrictedSectorsCount;
+
+    public Board(int size) {
+        this.setSize(size);
+        this.setRestrictedSectorsCount(4);
+        this.setRestrictedSectors();
+        this.setBoard();
+    }
+
+    // getters
+    public int getSize() {
+        return this.size;
+    }
+
+    public Sector[][] getBoard() {
+        return this.board;
+    }
+
+    public Coordinate[] getRestrictedSectors() {
+        return this.restrictedSectors;
+    }
+
+    public int getRestrictedSectorsCount() {
+        return this.restrictedSectorsCount;
+    }
+
+    // setters
+    public void setSize(int size) {
+        if (size < 0)
+            throw new IllegalArgumentException("Size must be positive");
+        this.size = size;
+        return;
+    }
+
+    public void setRestrictedSectorsCount(int restrictedSectorsCount) {
+        if (restrictedSectorsCount < 0)
+            throw new IllegalArgumentException("Restricted sectors count must be positive");
+        this.restrictedSectorsCount = restrictedSectorsCount;
+        return;
+    }
+
+    public void setRestrictedSectors() {
+        int restrictedSectorsCount = this.getRestrictedSectorsCount();
+
+        this.restrictedSectors = new Coordinate[restrictedSectorsCount];
+
+        // create the restricted sectors
+        // CUIDAR SE MAIS DE UM PONTO FOR GERADO NA MESMA POSICAO -- amaral
+        for (int i = 0; i < restrictedSectorsCount; i++) {
+            // randomnly generate two numbers between 0 and 8
+            int iCoordinate = (int) (Math.random() * 9); // -- Isso tava com 10, gerava de 0 a 9, mudei pra 9 -- amaral
+            int jCoordinate = (int) (Math.random() * 9);
+            this.restrictedSectors[i] = new Coordinate(iCoordinate, jCoordinate);
+        }
+
+        return;
+    }
+
+    public void setBoard() {
+        size = this.getSize();
+        this.board = new Sector[size][size];
+        boolean isRestricted = false;
+
+        // create the board
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                for (int x = 0; x < this.restrictedSectorsCount; x++) {
+                    if (i == this.restrictedSectors[x].getI() && j == this.restrictedSectors[x].getJ()) {
+                        isRestricted = true;
+                        break;
+                    }
+                }
+                this.board[i][j] = new Sector(i, j, isRestricted);
+                isRestricted = false;
+            }
+        }
+    }
+    // methods
+
+    public void drawBoard() {
+        size = this.getSize();
+        board = this.getBoard();
+
+        // draw the board
+        for (int i = 0; i < size; i++) {
+            // draw the top and bottom of the board
+            if (i == 0 || i == size) {
+                System.out.print("+----+");
+                for (int j = 1; j < size; j++) {
+                    System.out.print("----+");
+                }
+                System.out.println();
+            }
+
+            // draw the first side of the board
+            System.out.printf("| %s |", board[i][0].isRestricted() ? "XX" : "  ");
+
+            // draw the rest of the board
+            for (int j = 1; j < size; j++) {
+                System.out.printf(" %s |", board[i][j].isRestricted() ? "XX" : "  ");
+            }
+
+            System.out.println();
+            System.out.print("+----+");
+
+            // draw the bottom of the board
+            for (int j = 1; j < size; j++) {
+                System.out.print("----+");
+            }
+            System.out.println();
+
+        }
+    }
+}
---
+++ Board/Sector.java
@@ -0,0 +1,52 @@
+package Board;
+
+public class Sector {
+    private int i;
+    private int j;
+    private boolean restricted; // não tem comportamento diferente
+
+    public Sector(int i, int j, Boolean restricted) {
+        this.setI(i);
+        this.setJ(j);
+        this.setRestricted(restricted);
+    }
+
+    // getters
+    public int getI() {
+        return this.i;
+    }
+
+    public int getJ() {
+        return this.j;
+    }
+
+    public boolean isRestricted() {
+        return this.restricted;
+    }
+
+    // setters
+    public void setI(int i) {
+        if (i < 0)
+            throw new IllegalArgumentException("I must be positive");
+        this.i = i;
+        return;
+    }
+
+    public void setJ(int j) {
+        if (j < 0)
+            throw new IllegalArgumentException("J must be positive");
+        this.j = j;
+        return;
+    }
+
+    public void setRestricted(boolean value) {
+        this.restricted = value;
+        return;
+    }
+
+    // methods
+    public void draw() {
+        return;
+    }
+
+}
