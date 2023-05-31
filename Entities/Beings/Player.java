1873f2e75b49fb9195266e3cd5fe2d98f89180b1
vinícius <v.abreu.fontoura@gmail.com>
2023-05-31 14:31:29 -0300

    [wip] implementing player movement

--- Entities/Beings/Player.java
+++ Entities/Beings/Player.java
@@ -2,42 +2,48 @@ package Entities.Beings;
 
 import Entities.Coordinate;
 import Entities.Entity;
+import java.awt.event.KeyEvent;
 
-public class Player extends Entity implements Movement 
-{
+public class Player extends Entity implements Movement {
     // attributes
     private int playerNum;
 
     // constructor
-    public Player (int playerNum, Coordinate position)
-    {
+    public Player(int playerNum, Coordinate position) {
         super(position);
         this.setPlayerNum(playerNum);
     }
 
     // getters
-    int getPlayerNum()
-    {
+    int getPlayerNum() {
         return this.playerNum;
     }
 
     // setters
-    void setPlayerNum(int playerNum)
-    {
+    void setPlayerNum(int playerNum) {
         if (playerNum < 1 || playerNum > 4)
             throw new IllegalArgumentException("Player number must be between 1 and 4");
-        
+
         this.playerNum = playerNum;
     }
 
     // methods
-    public void draw()
-    {
+    public void draw() {
         System.out.printf("F%d", this.playerNum);
     }
 
-    public void move()
-    {
-        // TODO
+    public void move(KeyEvent e) {
+        // check if the input was an arrow keyCode
+        int keyCode = e.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.VK_UP:
+                Coordinate positionToGo = new Coordinate(this.position.getI() - 1, this.position.getJ());
+                break;
+
+            default:
+                // TODO: show error message and try again
+                break;
+        }
+
     }
 }
