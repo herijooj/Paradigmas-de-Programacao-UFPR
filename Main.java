faab98922bcc2bb82b6c265392764f16a4f93ed0
HeriJooj <heric_camargo@hotmail.com>
2023-05-31 11:11:49 -0300

    [add] Main title screen

--- Main.java
+++ Main.java
@@ -15,14 +15,57 @@ public class Main {
         return (int) (Math.random() * (max - min + 1) + min);
     }
 
+    // ascii art title screen
+    public static void asciiArt() {
+        String[] pattern = {
+            ".........................................................................",
+            ".######...####...##..##..######..........##..##..######..##...##...####..",
+            ".##......##..##..##.##...##..............###.##..##......##...##..##.....",
+            ".####....######..####....####............##.###..####....##.#.##...####..",
+            ".##......##..##..##.##...##..............##..##..##......#######......##.",
+            ".##......##..##..##..##..######..........##..##..######...##.##....####..",
+            "........................................................................."
+        };
+        for (int i = 0; i < pattern.length; i++) {
+            System.out.println(pattern[i]);
+        }
+    }
+
+    // function to clear screen
+    public static void flushScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // main function ====================================
     public static void main(String[] args) {
-        // System.out.println(Cores.ANSI_RED + "Red text" + Cores.ANSI_RESET);
-        // System.out.println(Cores.ANSI_BLUE + "Blue text" + Cores.ANSI_RESET);
-        // System.out.println(Cores.ANSI_YELLOW_BACKGROUND + "Yellow background"
-        // + Cores.ANSI_RESET);
+
+        // title screen ------------------------------------
+
+        flushScreen();
+        asciiArt();
+        System.out.println("Welcome to the game!");
+        System.out.println("Press any key to continue...");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        flushScreen();
+
+        // game loop ---------------------------------------
 
         Board board = new Board(9);
-
         board.drawBoard();
+
+        // 25 turns
+        for (int i = 0; i < 25; i++) {
+            if (i == 24) {
+                flushScreen();
+                asciiArt(); // substitute for game over screen
+                break;
+            }
+            System.out.println("Turn " + (i + 1) + " of 25");
+            scanner.nextLine();
+            //flushScreen();
+            board.drawBoard();
+        }
     }
 }
