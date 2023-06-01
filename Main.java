a4a9af8ef0b44366bc251fbe175d3c05664fecf0
HeriJooj <heric_camargo@hotmail.com>
2023-06-01 19:50:12 -0300

    [add] writing the enemy turn

--- Main.java
+++ Main.java
@@ -45,6 +45,21 @@ public class Main {
         }
     }
 
+    //placeholder for game win
+    public static void gameWin() {
+        String[] pattern = {
+            "  __          __  _                            _ ",
+            "  \\ \\        / / | |                          | |",
+            "   \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___  | |",
+            "    \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | |",
+            "     \\  /\\  /  __/ | (_| (_) | | | | | |  __/ |_|",
+            "      \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___| (_)"
+        };
+        for (int i = 0; i < pattern.length; i++) {
+            System.out.println(pattern[i]);
+        }
+    }
+
 
     // function to clear screen
     public static void flushScreen() {
@@ -67,19 +82,41 @@ public class Main {
 
         // game loop ---------------------------------------
 
+        // create board
         Board board = new Board(9);
+        // create player
+
+
         board.drawBoard();
 
         // 25 turns
         for (int i = 0; i < 25; i++) {
+            // if it is the last turn, game over
             if (i == 24) {
                 flushScreen();
                 gameOver();
                 break;
             }
+
+            // player turn ==================================
+            System.out.println("Player action");
+            System.out.println("Press any key to continue...");
+            scanner.nextLine();
+            flushScreen();
+            board.drawBoard();
+
+            // Enemy turn ===================================
+            System.out.println("Enemy action");
+            
+            // Enemy movement
+            
+            // the enemies will move randomly
+            
+
+            board.drawBoard();
+
             System.out.println("Turn " + (i + 1) + " of 25");
             scanner.nextLine();
-            //flushScreen();
             board.drawBoard();
         }
     }
