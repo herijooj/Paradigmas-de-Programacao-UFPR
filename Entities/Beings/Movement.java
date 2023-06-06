2c8e780978278e65e650860f9fc52f0ddcf7529c
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-06-06 16:40:02 -0300

    [wip] maybe we wont use it

--- Entities/Beings/Movement.java
+++ Entities/Beings/Movement.java
@@ -1,11 +1,13 @@
 package Entities.Beings;
 
 import java.awt.event.KeyEvent;
+import java.util.*;
 
 import Board.Sector;
+import Entities.Entity;
 
 // class
 public interface Movement {
     // methods
-    public void move(Sector[][] board, KeyEvent e, int direction);
+    public void move(Sector[][] board, LinkedList<Entity> Entities, KeyEvent e, int direction);
 }
