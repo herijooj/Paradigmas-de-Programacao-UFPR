79b949b2fc12b41580b92f6e220e80cf346868b9
Pedro <amaral1@ufpr.br>
2023-06-08 16:20:42 -0300

    [add] state attribute

--- Entities/Beings/FakeNews.java
+++ Entities/Beings/FakeNews.java
@@ -12,15 +12,28 @@ import Board.Sector;
 public abstract class FakeNews extends Entity {
 
     // attributes
+    protected String state;
 
     // constructor
     public FakeNews(Coordinate position) {
         super(position);
+        this.setState("alive");
     }
 
     // getters
+    public String getState()
+    {
+        return this.state;
+    }
 
     // setters
+    public void setState(String state)
+    {
+        if (state != "alive" && state != "dead" && state != "outOfGame")
+            throw new IllegalArgumentException("Invalid state, valids: [alive, dead and outOfGame]");
+        else
+            this.state = state;
+    }
 
     // methods
     public boolean move(Sector[][] board, LinkedList<FakeNews> fakeNews, KeyEvent e, int direction) {
