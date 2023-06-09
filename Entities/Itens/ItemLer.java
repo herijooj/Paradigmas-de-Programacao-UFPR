d3386f9e29849493f079115fe16e7399fae5f90f
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-06-09 11:28:39 -0300

    [wip] item ability

--- Entities/Itens/ItemLer.java
+++ Entities/Itens/ItemLer.java
@@ -1,6 +1,9 @@
 package Entities.Itens;
 
 import Entities.Coordinate;
+
+import java.util.LinkedList;
+
 import Board.*;
 import Entities.Beings.*;
 
@@ -8,30 +11,34 @@ import Entities.Beings.*;
 public class ItemLer extends ItemCharacteristics {
 
     // attributes
-    
+
     // constructor
-    public ItemLer (Coordinate position) 
-    {
+    public ItemLer(Coordinate position) {
         super(position);
     }
-    
+
     // getters
-    
+
     // setters
 
     // methods
-    public void itemAbility(Board board, Player player) 
-    {
+    public void itemAbility(Board board, Player player) {
+
         // escolhe um inimigo para deletar
+        // percorre o board e procura por um inimigo aleatorio
+        // seta o state do inimigo para 'dead'
+
+        LinkedList<FakeNews> enemies = board.getFakeNews();
+        int randomIndex = (int) (Math.random() * enemies.size());
+        enemies.get(randomIndex).setState("dead");
+        return;
     }
 
-    public void draw()
-    {
+    public void draw() {
         // TODO
     }
 
-    public String toString() 
-    {
+    public String toString() {
         return "Item Ler";
     }
 }
