a0b558153157ddf6693a324c48de0ac977269ef5
Heric Camargo <heric_camargo@hotmail.com>
2023-06-09 22:45:36 -0300

    [draft] start of itemBoato

--- Entities/Itens/ItemBoato.java
+++ Entities/Itens/ItemBoato.java
@@ -23,6 +23,28 @@ public class ItemBoato extends ItemCharacteristics {
     public void itemAbility(Board board, Player player) 
     {
         // proximo turno de forma aleatoria
+
+        // escolhe se o proximo turno sera do player ou do inimigo
+        int random = (int) (Math.random() * 2);
+
+        // se for 0, o proximo turno sera do player
+        if (random == 0) 
+        {
+            // escolhe uma direcao aleatoria
+            random = (int) (Math.random() * 4);
+            // escolhe um player aleatorio
+            int randomPlayer = (int) (Math.random() * board.getPlayers().size());
+            board.movePlayer(random, randomPlayer);
+        }
+        // se for 1, o proximo turno sera do inimigo
+        else 
+        {
+            // escolhe uma direcao aleatoria
+            random = (int) (Math.random() * 4);
+            // escolhe um inimigo aleatorio
+            int randomEnemy = (int) (Math.random() * board.getFakeNews().size());
+            board.moveIndividualFakeNews(randomEnemy);
+        }
     }
 
     public void draw()
