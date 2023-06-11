e4f91a5b69c28df30aff8cdeb7c2c0b3b610b267
Pedro <amaral1@ufpr.br>
2023-06-11 13:51:29 -0300

    [fix] Item fugir works

--- Entities/Itens/ItemFugir.java
+++ Entities/Itens/ItemFugir.java
@@ -27,13 +27,61 @@ public class ItemFugir extends ItemCharacteristics {
     }
 
     /**
+     * Verifies if a given position is valid
+     *
+     * returns true if it is, false if it isnt
+     */
+    public boolean validPosition(Board board, int input)
+    {
+        int i, j;
+
+        // Verifies if it is in the board
+        if (input < 0 || input > 88)
+            return false;
+
+        // Verifies if the selected sector is invalid
+        if (board.getBoard()[input / 10][input % 10].getSectorState() != "")
+            return false;
+
+        // If it got here, means its available
+        return true;
+    }
+
+    /**
      * This item lets the player choose a position to go
      * 
      * @param board  the board
      * @param player the player
      */
-    public void itemAbility(Board board, Player player) {
+    public void itemAbility(Board board, Player player) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int input;
+        Coordinate newCoordinate;
 
+        System.out.println("Player " + player.getPlayerNum() + " type a position to go:");
+        System.out.println("Format: [LineNumber][ColumnNumber], example: 12");
+        input = sc.nextInt();
+        sc.nextLine();
+
+        while (!validPosition(board, input))
+        {
+            System.out.println("Cant move to that position, type again: ");
+            input = sc.nextInt();
+            sc.nextLine();
+        }
+
+        // Clear old position
+        board.getBoard()[player.getPosition().getI()][player.getPosition().getJ()].setSectorState("");
+
+        // Change to new position
+        newCoordinate = new Coordinate(input / 10, input % 10);
+        player.setPosition(newCoordinate);
+
+        // Update the sectorState
+        board.getBoard()[newCoordinate.getI()][newCoordinate.getJ()].setSectorState(player.toString());
+
+        System.out.println("Player " + player.getPlayerNum() + " moved!");
     }
 
     public String toString() {
