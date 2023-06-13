7da8059c39cfcf217e861c67472de259860b7c28
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-06-13 20:54:50 -0300

    [refactor] possible positions are now a method

--- Entities/Itens/ItemDenunciar.java
+++ Entities/Itens/ItemDenunciar.java
@@ -36,39 +36,52 @@ public class ItemDenunciar extends ItemCharacteristics {
 
         // create a list of positions, where the posistion has distance of 1 from the
         // player
-        LinkedList<Coordinate> positions = new LinkedList<Coordinate>();
-        positions.add(new Coordinate(player.getPosition().getI() + 1, player.getPosition().getJ())); // down
-        positions.add(new Coordinate(player.getPosition().getI() + 1, player.getPosition().getJ() - 1)); // down-left
-        positions.add(new Coordinate(player.getPosition().getI() + 1, player.getPosition().getJ() + 1)); // down-right
-        positions.add(new Coordinate(player.getPosition().getI() - 1, player.getPosition().getJ())); // up
-        positions.add(new Coordinate(player.getPosition().getI() - 1, player.getPosition().getJ() - 1)); // up-left
-        positions.add(new Coordinate(player.getPosition().getI() - 1, player.getPosition().getJ() + 1)); // up-right
-        positions.add(new Coordinate(player.getPosition().getI(), player.getPosition().getJ() + 1)); // right
-        positions.add(new Coordinate(player.getPosition().getI() - 1, player.getPosition().getJ() + 1)); // right-up
-        positions.add(new Coordinate(player.getPosition().getI() + 1, player.getPosition().getJ() + 1)); // right
-        positions.add(new Coordinate(player.getPosition().getI(), player.getPosition().getJ() - 1)); // left
-        positions.add(new Coordinate(player.getPosition().getI() - 1, player.getPosition().getJ() - 1)); // left-up
-        positions.add(new Coordinate(player.getPosition().getI() + 1, player.getPosition().getJ() - 1)); // left-down
+        LinkedList<Coordinate> positions = possiblePositions(player.getPosition());
 
         // for each position, check if there is a fakeNews
-        for (i = 0; i < positions.size(); i++)
-        {
-            for (j = 0; j < fakeNewsList.size(); j++)
-            {
+        for (i = 0; i < positions.size(); i++) {
+            for (j = 0; j < fakeNewsList.size(); j++) {
                 // If fakeNews is in the area
-                if (fakeNewsList.get(j).getPosition().getI() == positions.get(i).getI() && fakeNewsList.get(j).getPosition().getJ() == positions.get(i).getJ())
-                {
-                    if (fakeNewsList.get(j).getState() == "RecentlyAdded" || fakeNewsList.get(j).getState() == "alive")
-                    {
+                if (fakeNewsList.get(j).getPosition().getI() == positions.get(i).getI()
+                        && fakeNewsList.get(j).getPosition().getJ() == positions.get(i).getJ()) {
+                    if (fakeNewsList.get(j).getState() == "RecentlyAdded"
+                            || fakeNewsList.get(j).getState() == "alive") {
                         // Set state to outOfGame and setSectorState to nothing there
                         fakeNewsList.get(j).setState("outOfGame");
-                        board.getBoard()[fakeNewsList.get(j).getPosition().getI()][fakeNewsList.get(j).getPosition().getJ()].setSectorState("");
-                        System.out.printf("Fake news " + Cores.ANSI_RED + "%d " + Cores.ANSI_RESET + "eliminated! :)\n", j + 1);
+                        board.getBoard()[fakeNewsList.get(j).getPosition().getI()][fakeNewsList.get(j).getPosition()
+                                .getJ()].setSectorState("");
+                        System.out.printf("Fake news " + Cores.ANSI_RED + "%d " + Cores.ANSI_RESET + "eliminated! :)\n",
+                                j + 1);
                     }
                     break;
                 }
             }
         }
+    }
+
+    /**
+     * creates a list of possible positions to go
+     * based on the player's position
+     * 
+     * @param position the position of the player
+     * @return a list of possible positions
+     */
+    public LinkedList<Coordinate> possiblePositions(Coordinate position) {
+        LinkedList<Coordinate> positions = new LinkedList<Coordinate>();
+        positions.add(new Coordinate(position.getI() + 1, position.getJ())); // down
+        positions.add(new Coordinate(position.getI() + 1, position.getJ() - 1)); // down-left
+        positions.add(new Coordinate(position.getI() + 1, position.getJ() + 1)); // down-right
+        positions.add(new Coordinate(position.getI() - 1, position.getJ())); // up
+        positions.add(new Coordinate(position.getI() - 1, position.getJ() - 1)); // up-left
+        positions.add(new Coordinate(position.getI() - 1, position.getJ() + 1)); // up-right
+        positions.add(new Coordinate(position.getI(), position.getJ() + 1)); // right
+        positions.add(new Coordinate(position.getI() - 1, position.getJ() + 1)); // right-up
+        positions.add(new Coordinate(position.getI() + 1, position.getJ() + 1)); // right
+        positions.add(new Coordinate(position.getI(), position.getJ() - 1)); // left
+        positions.add(new Coordinate(position.getI() - 1, position.getJ() - 1)); // left-up
+        positions.add(new Coordinate(position.getI() + 1, position.getJ() - 1)); // left-down
+
+        return positions;
     }
 
     public void draw() {
