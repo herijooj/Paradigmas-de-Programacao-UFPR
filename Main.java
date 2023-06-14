580b8e7a14c69d11d8ba09e81c17ebdb6423e572
Heric Camargo <heric_camargo@hotmail.com>
2023-06-14 15:59:37 -0300

    [remove] print redudant

--- Main.java
+++ Main.java
@@ -278,7 +278,7 @@ public class Main {
 
                         board.movePlayer(j, input);
                         increaseTurnAndDrawBoard(i, board);
-                        System.out.printf("Player " + Cores.ANSI_GREEN + "J%d " + Cores.ANSI_RESET + "moved!\n", j + 1);
+                        //System.out.printf("Player " + Cores.ANSI_GREEN + "J%d " + Cores.ANSI_RESET + "moved!\n", j + 1);
                         printPositionUpdate(board, j, "player");
                         checkForPlayersDeaths(board, i, j);
                     }
@@ -291,7 +291,7 @@ public class Main {
 
                     board.movePlayer(j, input);
                     increaseTurnAndDrawBoard(i, board);
-                    System.out.printf("Player " + Cores.ANSI_GREEN + "J%d " + Cores.ANSI_RESET + "moved!\n", j + 1);
+                    //System.out.printf("Player " + Cores.ANSI_GREEN + "J%d " + Cores.ANSI_RESET + "moved!\n", j + 1);
                     printPositionUpdate(board, j, "player");
                     itemUsed = false;
                     checkForPlayersDeaths(board, i, j);
@@ -327,7 +327,7 @@ public class Main {
                     board.moveIndividualFakeNews(x);
 
                 increaseTurnAndDrawBoard(i, board);
-                System.out.println("--> " + Cores.ANSI_RED + " Fake news " + (x + 1) + Cores.ANSI_RESET + " moved");
+                //System.out.println("--> " + Cores.ANSI_RED + " Fake news " + (x + 1) + Cores.ANSI_RESET + " moved");
                 printPositionUpdate(board, x, "fakeNews");
 
                 // Check for fakeNews deaths
