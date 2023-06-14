8e72ce55ec85825be5d2c9df6bdd8c211f50e053
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2023-06-13 21:21:23 -0300

    [add] move javadocs

--- Entities/Beings/Movement.java
+++ Entities/Beings/Movement.java
@@ -5,5 +5,13 @@ import Board.Board;
 // class
 public interface Movement {
     // methods
+
+    /**
+     * it will move the entity in the given direction
+     * 
+     * @param board
+     * @param direction
+     * @return
+     */
     public String move(Board board, int direction);
 }
