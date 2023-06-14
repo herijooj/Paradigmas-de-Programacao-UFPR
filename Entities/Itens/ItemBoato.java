604753c4de31c8e7ecb0c7ff6751172a622e830d
Pedro Amaral <amaral1@ufpr.br>
2023-06-14 09:05:13 -0300

    [refactor]

--- Entities/Itens/ItemBoato.java
+++ Entities/Itens/ItemBoato.java
@@ -7,16 +7,10 @@ import Entities.Beings.*;
 // class
 public class ItemBoato extends ItemCharacteristics {
 
-    // attributes
-
-    // constructor
+    // Constructor
     public ItemBoato(Coordinate position) {
         super(position);
     }
-
-    // getters
-
-    // setters
 
     /**
      * it will choose a random direction for the player to move in the next turn
@@ -26,9 +20,6 @@ public class ItemBoato extends ItemCharacteristics {
      */
     public void itemAbility(Board board, Player player) {
         // Used automatically in the movePlayers function
-    }
-
-    public void draw() {
     }
 
     public String toString() {
