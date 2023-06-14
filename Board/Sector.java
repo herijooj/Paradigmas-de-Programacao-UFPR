822fadaea55f37fbef1189c465013c5ac88a55f7
Heric Camargo <heric_camargo@hotmail.com>
2023-06-14 16:18:02 -0300

    [remove] attributes i and j

--- Board/Sector.java
+++ Board/Sector.java
@@ -1,29 +1,35 @@
 package Board;
 
+import Entities.Coordinate;
+
 public class Sector {
     
     // Attributes
 
-    private int i;
-    private int j;
+    // private int i;
+    // private int j;
+    private Coordinate coordinate;
     private String sectorState;
 
     // Constructor 
 
-    public Sector(int i, int j, String sectorState) {
-        this.setI(i);
-        this.setJ(j);
+    public Sector(Coordinate coordinate, String sectorState) {
+        this.setCoordinate(coordinate);
         this.setSectorState(sectorState);
     }
 
     // Getters
 
-    public int getI() {
-        return this.i;
-    }
+    // public int getI() {
+    //     return this.i;
+    // }
 
-    public int getJ() {
-        return this.j;
+    // public int getJ() {
+    //     return this.j;
+    // }
+
+    public Coordinate getCoordinate() {
+        return this.coordinate;
     }
 
     /**
@@ -36,17 +42,22 @@ public class Sector {
 
     // Setters
 
-    public void setI(int i) {
-        if (i < 0)
-            throw new IllegalArgumentException("I must be positive");
-        this.i = i;
-        return;
-    }
+    // public void setI(int i) {
+    //     if (i < 0)
+    //         throw new IllegalArgumentException("I must be positive");
+    //     this.i = i;
+    //     return;
+    // }
 
-    public void setJ(int j) {
-        if (j < 0)
-            throw new IllegalArgumentException("J must be positive");
-        this.j = j;
+    // public void setJ(int j) {
+    //     if (j < 0)
+    //         throw new IllegalArgumentException("J must be positive");
+    //     this.j = j;
+    //     return;
+    // }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
         return;
     }
 
