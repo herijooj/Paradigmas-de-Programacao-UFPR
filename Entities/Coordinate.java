1c7abafa7c6d84239c27ade58429ceb653a9b3bd
Heric Camargo <heric_camargo@hotmail.com>
2023-06-15 23:40:11 -0300

    [remove] useless functions

--- Entities/Coordinate.java
+++ Entities/Coordinate.java
@@ -34,37 +34,6 @@ public class Coordinate {
     // Methods
 
     /**
-     * Calculates the distance between the current Coordinate object and the given Coordinate object.
-     *
-     * @param coordinate The Coordinate object to calculate the distance to.
-     * @return The distance between the two Coordinate objects as an integer.
-     */
-    public int calculateDistance(Coordinate coordinate) {
-        int i = this.getI();
-        int j = this.getJ();
-        int i2 = coordinate.getI();
-        int j2 = coordinate.getJ();
-
-        int distance = (int) Math.sqrt(Math.pow(i2 - i, 2) + Math.pow(j2 - j, 2));
-
-        return distance;
-    }
-
-    /**
-     * Checks if the current Coordinate object is valid within a 10x10 grid.
-     *
-     * @return true if the Coordinate object is valid, false otherwise.
-     */
-    public boolean Isvalid() {
-        int i = this.getI();
-        int j = this.getJ();
-
-        if (i < 0 || i > 9 || j < 0 || j > 9)
-            return false;
-        return true;
-    }
-
-    /**
      * Generates a random coordinate within a 10x10 grid.
      *
      * @return A randomly generated Coordinate object.
